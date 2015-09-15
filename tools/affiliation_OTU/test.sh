72ecc4ada57cab547a931119d193224401161ee2
Frederic Escudie <fescudie@armengaud.toulouse.inra.fr>
2015-09-15 16:30:35 +0200

    Move to tags structure.

--- tools/affiliation_OTU/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PATH=../../bin:$PATH
-export PYTHONPATH=../../bin:$PYTHONPATH
-
-./affiliation_OTU.py --reference data/SILVA_119_SSURef_Nr99_tax_silva.fasta \
-                     --input-biom data/swarm.biom --input-fasta data/swarm.fasta \
-                     --output-biom test/affiliation.biom \
-                     --summary test/summary.tsv --log-file test/aff.log
--- tools/clusters_stat/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH=../../bin:$PATH
-export PYTHONPATH=../../bin:$PYTHONPATH
-./clusters_stat.py --input-biom data/swarm.biom \
-                   --output-file test/clusters_metrics.html \
-                   --log-file test/log.txt
