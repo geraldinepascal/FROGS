d28aef2948e3eec38309eab41ba1b9bd7cdf6691
maria <maria.bernard@jouy.inra.Fr>
2016-06-15 15:56:41 +0200

    update version

--- tools/filters/filters.py
+++ tools/filters/filters.py
@@ -19,7 +19,7 @@
 __author__ = 'Katia Vidal - Team NED Toulouse AND Frederic Escudie - Plateforme bioinformatique Toulouse'
 __copyright__ = 'Copyright (C) 2015 INRA'
 __license__ = 'GNU General Public License'
-__version__ = '1.3.1'
+__version__ = '1.3.2'
 __email__ = 'frogs@toulouse.inra.fr'
 __status__ = 'prod'
 
