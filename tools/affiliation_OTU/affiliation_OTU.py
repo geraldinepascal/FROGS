geraldinepascal/FROGS

commit ecad5e173b857c3e8fbfecace4013f3767df5286

make RDP affiliation optionnal, update Readme with tar datatype

tools/affiliation_OTU/affiliation_OTU.py
@@ -19,7 +19,7 @@
 __author__ = 'Maria Bernard INRA - SIGENAE AND Frederic Escudie - Plateforme bioinformatique Toulouse'
 __copyright__ = 'Copyright (C) 2015 INRA'
 __license__ = 'GNU General Public License'
-__version__ = '0.10.0'
+__version__ = '0.11.0'
 __email__ = 'frogs@toulouse.inra.fr'
 __status__ = 'prod'
 
@@ -114,9 +114,9 @@ class AddAffiliation2Biom(Cmd):
         @param out_tsv: [str] Path to output TSV file.
         """
         argument="-f " + ref + " -i " + in_biom + " -o " + out_biom
-        if blast_list is not None:
+        if len(blast_list) > 0 :
             argument += " -b " + " ".join(blast_list)
-        if rdp_list is not None:
+        if len(rdp_list) > 0 :
             argument += " -r " + " ".join(rdp_list)
         Cmd.__init__( self,
                       'addAffiliation2biom.py',
@@ -315,9 +315,10 @@ def parallel_submission( function, inputs, outputs, logs, cpu_used, reference, m
 if __name__ == "__main__":
     # Manage parameters
     parser = argparse.ArgumentParser(description="Taxonomic affiliation of each OTU's seed by RDPtools and BLAST.")
-    parser.add_argument( '-p', '--nb-cpus', type=int, default=1, help="The maximum number of CPUs used. [Default: %(default)s]")
-    parser.add_argument( '-m', '--java-mem', type=int, default=2, help="Java memory allocation in Go. [Default: %(default)s]")
-    parser.add_argument( '-t', '--taxonomy-ranks', nargs='*', default=["Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species"], help='The ordered ranks levels present in the reference databank. [Default: %(default)s]' )
+    parser.add_argument( '-p', '--nb-cpus', type=int, default=1, help="The maximum number of CPUs used.(default 1)")
+    parser.add_argument( '-m', '--java-mem', type=int, default=2, help="Java memory allocation in Go.(default 2)")
+    parser.add_argument( '-t', '--taxonomy-ranks', nargs='*', default=["Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species"], help='The ordered ranks levels present in the reference databank.' )
+    parser.add_argument('--rdp', default=False,  action='store_true',  help="Use RDP classifier to affiliate OTU")
     parser.add_argument( '-d', '--debug', default=False, action='store_true', help="Keep temporary files to debug program.")
     parser.add_argument( '-v', '--version', action='version', version=__version__)
     # Inputs
@@ -350,18 +351,20 @@ if __name__ == "__main__":
 
         if args.nb_cpus == 1 or nb_seq < 100:
             # RDP
-            rdp_out_list.append( tmpFiles.add(os.path.basename(args.input_fasta) + ".rdp") )
-            process_rdp( args.input_fasta, rdp_out_list[0], args.log_file, args.reference, args.java_mem)
-            # Blast
+            if args.rdp:
+                rdp_out_list.append( tmpFiles.add(os.path.basename(args.input_fasta) + ".rdp") )
+                process_rdp( args.input_fasta, rdp_out_list[0], args.log_file, args.reference, args.java_mem)
+            # BLAST
             blast_out_list.append( tmpFiles.add(os.path.basename(args.input_fasta) + ".blast") )
             Blast(args.reference, args.input_fasta, blast_out_list[0], 1).submit(args.log_file)
         else:
             # RDP
             split_fasta(args.input_fasta, tmpFiles, max(1, int(args.nb_cpus/3)), fasta_rdp_list, args.log_file)
-            rdp_out_list = [tmpFiles.add(os.path.basename(current_fasta) + ".rdp") for current_fasta in fasta_rdp_list]
-            log_rdp_list = [tmpFiles.add(os.path.basename(current_fasta) + "_rdp.log") for current_fasta in fasta_rdp_list]
-            parallel_submission( process_rdp, fasta_rdp_list, rdp_out_list, log_rdp_list, len(fasta_rdp_list), args.reference, args.java_mem )
-            # Blast
+            if args.rdp:
+                rdp_out_list = [tmpFiles.add(os.path.basename(current_fasta) + ".rdp") for current_fasta in fasta_rdp_list]
+                log_rdp_list = [tmpFiles.add(os.path.basename(current_fasta) + "_rdp.log") for current_fasta in fasta_rdp_list]
+                parallel_submission( process_rdp, fasta_rdp_list, rdp_out_list, log_rdp_list, len(fasta_rdp_list), args.reference, args.java_mem )
+            # BLAST
             blast_out_list.append( tmpFiles.add(os.path.basename(args.input_fasta) + ".blast") )
             log_blast_list.append( tmpFiles.add(os.path.basename(args.input_fasta) + "_blast.log") )
             Blast(args.reference, args.input_fasta, blast_out_list[0], args.nb_cpus).submit(log_blast_list[0])

tools/affiliation_OTU/affiliation_OTU.xml
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
-<tool id="FROGS_affiliation_OTU" name="FROGS Affiliation OTU" version="0.7.0">
+<tool id="FROGS_affiliation_OTU" name="FROGS Affiliation OTU" version="0.8.0">
 	<description>Step 4 in metagenomics analysis : Taxonomic affiliation of each OTU's seed by RDPtools and BLAST</description>
 	<command interpreter="python2.7">
 		affiliation_OTU.py 
@@ -24,8 +24,11 @@
 		                       --input-fasta $fasta_sequences
 		                       --output-biom $biom_affiliation 
 		                       --summary $summary 
-		                       --nb-cpus $nb_cpu 
-		                       --java-mem $mem
+		                       --nb-cpus $nb_cpu
+		                       --java-mem $mem 
+		                       #if $rdp
+		                       		--rdp
+		                       	#end if
 	</command>
 	<inputs>
 		<!-- JOB Parameter -->
@@ -38,6 +41,7 @@
           		<column name="value" index="1"/>
        		</options>
    		</param> 
+   		<param name="rdp" type="boolean" label="Also perform RDP assignation?" help="Taxonomy affiliation will be perform thanks to Blast. This option allow you to perform it also with RDP classifier (default No)" />
         <!-- Files -->
         <param format="fasta" name="fasta_sequences" type="data" label="OTU seed sequence" help="OTU sequences (format: fasta)."/>
         <param format="biom1" name="biom_abundance" type="data" label="Abundance file" help="OTU abundances (format: BIOM)."/>

tools/affiliation_OTU/test.sh
@@ -8,7 +8,15 @@ then
     mkdir test
 fi
 
+#Blast
+echo "#Blast"
 ./affiliation_OTU.py --reference data/db.fasta \
                      --input-biom data/swarm.biom --input-fasta data/swarm.fasta \
-                     --output-biom test/affiliation.biom \
-                     --summary test/summary.html --log-file test/aff.log
+                     --output-biom test/Blast_affiliation.biom \
+                     --summary test/Blast_summary.html --log-file test/Blast_aff.log
+#Blast and RDP
+echo "#Blast and RDP"
+./affiliation_OTU.py --reference data/db.fasta --rdp\
+                     --input-biom data/swarm.biom --input-fasta data/swarm.fasta \
+                     --output-biom test/Blast_RDP_affiliation.biom \
+                     --summary test/Blast_RDP_summary.html --log-file test/Blast_RDP_aff.log

commit 01ed4e0909e7a9ba309cf30214a4e1fcbce7780d

Fix regression in help introduced by the commit ecad5e173b857c3e8fbfecace4013f3767df5286.

tools/affiliation_OTU/affiliation_OTU.py
@@ -315,9 +315,9 @@ def parallel_submission( function, inputs, outputs, logs, cpu_used, reference, m
 if __name__ == "__main__":
     # Manage parameters
     parser = argparse.ArgumentParser(description="Taxonomic affiliation of each OTU's seed by RDPtools and BLAST.")
-    parser.add_argument( '-p', '--nb-cpus', type=int, default=1, help="The maximum number of CPUs used.(default 1)")
-    parser.add_argument( '-m', '--java-mem', type=int, default=2, help="Java memory allocation in Go.(default 2)")
-    parser.add_argument( '-t', '--taxonomy-ranks', nargs='*', default=["Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species"], help='The ordered ranks levels present in the reference databank.' )
+    parser.add_argument( '-p', '--nb-cpus', type=int, default=1, help="The maximum number of CPUs used. [Default: %(default)s]")
+    parser.add_argument( '-m', '--java-mem', type=int, default=2, help="Java memory allocation in Go. [Default: %(default)s]")
+    parser.add_argument( '-t', '--taxonomy-ranks', nargs='*', default=["Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species"], help='The ordered ranks levels present in the reference databank. [Default: %(default)s]' )
     parser.add_argument('--rdp', default=False,  action='store_true',  help="Use RDP classifier to affiliate OTU")
     parser.add_argument( '-d', '--debug', default=False, action='store_true', help="Keep temporary files to debug program.")
     parser.add_argument( '-v', '--version', action='version', version=__version__)