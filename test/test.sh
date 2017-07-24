601e7f752f637bc4a16af0cd50d123ec11789350
mariabernard <maria.bernard@inra.fr>
2017-07-24 15:24:21 +0200

    update test.sh

--- test/test.sh
+++ test/test.sh
@@ -23,198 +23,198 @@ then
 fi
 
 
-#~ echo "Step preprocess `date`"
-#~ 
-#~ preprocess.py illumina \
- #~ --min-amplicon-size 380 --max-amplicon-size 460 \
- #~ --five-prim-primer GGCGVACGGGTGAGTAA --three-prim-primer GTGCCAGCNGCNGCGG \
-  #~ --R1-size 250 --R2-size 250 --expected-amplicon-size 420 \
- #~ --input-archive $frogs_dir/test/data/test_dataset.tar.gz \
- #~ --output-dereplicated $out_dir/01-prepro.fasta \
- #~ --output-count $out_dir/01-prepro.tsv \
- #~ --summary $out_dir/01-prepro.html \
- #~ --log-file $out_dir/01-prepro.log \
- #~ --nb-cpus $nb_cpu --mismatch-rate 0.15
- #~ 
-#~ if [ $? -ne 0 ]
-#~ then
-	#~ echo "Error in preprocess" >&2
-	#~ exit 1;
-#~ fi
+echo "Step preprocess `date`"
+
+preprocess.py illumina \
+ --min-amplicon-size 380 --max-amplicon-size 460 \
+ --five-prim-primer GGCGVACGGGTGAGTAA --three-prim-primer GTGCCAGCNGCNGCGG \
+  --R1-size 250 --R2-size 250 --expected-amplicon-size 420 \
+ --input-archive $frogs_dir/test/data/test_dataset.tar.gz \
+ --output-dereplicated $out_dir/01-prepro.fasta \
+ --output-count $out_dir/01-prepro.tsv \
+ --summary $out_dir/01-prepro.html \
+ --log-file $out_dir/01-prepro.log \
+ --nb-cpus $nb_cpu --mismatch-rate 0.15
+ 
+if [ $? -ne 0 ]
+then
+	echo "Error in preprocess" >&2
+	exit 1;
+fi
 
 
-#~ echo "Step clustering `date`"
+echo "Step clustering `date`"
 
-#~ clustering.py \
- #~ --distance 3 \
- #~ --denoising \
- #~ --input-fasta $out_dir/01-prepro.fasta \
- #~ --input-count $out_dir/01-prepro.tsv \
- #~ --output-biom $out_dir/02-clustering.biom \
- #~ --output-fasta $out_dir/02-clustering.fasta \
- #~ --output-compo $out_dir/02-clustering_compo.tsv \
- #~ --log-file $out_dir/02-clustering.log \
- #~ --nb-cpus $nb_cpu
-#~ 
-#~ if [ $? -ne 0 ]
-#~ then
-	#~ echo "Error in clustering" >&2
-	#~ exit 1;
-#~ fi
-#~ 
-#~ 
-#~ echo "Step remove_chimera `date`"
-#~ 
-#~ remove_chimera.py \
- #~ --input-fasta $out_dir/02-clustering.fasta \
- #~ --input-biom $out_dir/02-clustering.biom \
- #~ --non-chimera $out_dir/03-chimera.fasta \
- #~ --out-abundance $out_dir/03-chimera.biom \
- #~ --summary $out_dir/03-chimera.html \
- #~ --log-file $out_dir/03-chimera.log \
- #~ --nb-cpus $nb_cpu
- #~ 
-#~ if [ $? -ne 0 ]
-#~ then
-	#~ echo "Error in remove_chimera" >&2
-	#~ exit 1;
-#~ fi
-#~ 
-#~ 
-#~ echo "Step filters `date`"
-#~ 
-#~ filters.py \
- #~ --min-abundance 0.00005 \
- #~ --min-sample-presence 3\
- #~ --input-biom $out_dir/03-chimera.biom \
- #~ --input-fasta $out_dir/03-chimera.fasta \
- #~ --output-fasta $out_dir/04-filters.fasta \
- #~ --output-biom $out_dir/04-filters.biom \
- #~ --excluded $out_dir/04-filters.excluded \
- #~ --summary $out_dir/04-filters.html \
- #~ --log-file $out_dir/04-filters.log 
-#~ 
-#~ if [ $? -ne 0 ]
-#~ then
-	#~ echo "Error in filters" >&2
-	#~ exit 1;
-#~ fi
-#~ 
-#~ 
-#~ echo "Step affiliation_OTU `date`"
-#~ 
-#~ affiliation_OTU.py \
- #~ --reference $frogs_dir/test/data/db.fasta \
- #~ --input-fasta $out_dir/04-filters.fasta \
- #~ --input-biom $out_dir/04-filters.biom \
- #~ --output-biom $out_dir/04-affiliation.biom \
- #~ --summary $out_dir/04-affiliation.html \
- #~ --log-file $out_dir/04-affiliation.log \
- #~ --nb-cpus $nb_cpu --java-mem $java_mem \
- #~ --rdp
-#~ 
-#~ if [ $? -ne 0 ]
-#~ then
-	#~ echo "Error in affiliation_OTU" >&2
-	#~ exit 1;
-#~ fi
-#~ 
-#~ 
-#~ echo "Step clusters_stat `date`"
-#~ 
-#~ clusters_stat.py \
- #~ --input-biom $out_dir/04-affiliation.biom \
- #~ --output-file $out_dir/05-clustersStat.html \
- #~ --log-file $out_dir/05-clustersStat.log
-#~ 
-#~ if [ $? -ne 0 ]
-#~ then
-	#~ echo "Error in clusters_stat" >&2
-	#~ exit 1;
-#~ fi
-#~ 
-#~ 
-#~ echo "Step affiliations_stat `date`"
-#~ 
-#~ affiliations_stat.py \
- #~ --input-biom $out_dir/04-affiliation.biom \
- #~ --output-file $out_dir/06-affiliationsStat.html \
- #~ --log-file $out_dir/06-affiliationsStat.log \
- #~ --tax-consensus-tag "blast_taxonomy" \
- #~ --identity-tag "perc_identity" \
- #~ --coverage-tag "perc_query_coverage" \
- #~ --multiple-tag "blast_affiliations" \
- #~ --rarefaction-ranks Family Genus Species
-#~ 
-#~ if [ $? -ne 0 ]
-#~ then
-	#~ echo "Error in affiliations_stat" >&2
-	#~ exit 1;
-#~ fi
-#~ 
-#~ 
-#~ echo "Step biom_to_tsv `date`"
-#~ 
-#~ biom_to_tsv.py \
- #~ --input-biom $out_dir/04-affiliation.biom \
- #~ --input-fasta $out_dir/04-filters.fasta \
- #~ --output-tsv $out_dir/07-biom2tsv.tsv \
- #~ --output-multi-affi $out_dir/07-biom2tsv.multi \
- #~ --log-file $out_dir/07-biom2tsv.log
-#~ 
-#~ if [ $? -ne 0 ]
-#~ then
-	#~ echo "Error in biom_to_tsv" >&2
-	#~ exit 1;
-#~ fi
-#~ 
-#~ echo "Step biom_to_stdBiom `date`"
-#~ 
-#~ 
-#~ biom_to_stdBiom.py \
- #~ --input-biom $out_dir/04-affiliation.biom \
- #~ --output-biom $out_dir/08-affiliation_std.biom \
- #~ --output-metadata $out_dir/08-affiliation_multihit.tsv \
- #~ --log-file $out_dir/08-biom2stdbiom.log
-#~ 
-#~ if [ $? -ne 0 ]
-#~ then
-	#~ echo "Error in biom_to_stdBiom" >&2
-	#~ exit 1;
-#~ fi
-#~ 
-#~ echo "Step tsv_to_biom `date`"
-#~ 
-#~ 
-#~ tsv_to_biom.py \
- #~ --input-tsv $out_dir/07-biom2tsv.tsv \
- #~ --input-multi-affi $out_dir/07-biom2tsv.multi \
- #~ --output-biom $out_dir/09-tsv2biom.biom \
- #~ --output-fasta $out_dir/09-tsv2biom.fasta \
- #~ --log-file $out_dir/09-tsv2biom.log 
-#~ 
-#~ if [ $? -ne 0 ]
-#~ then
-	#~ echo "Error in tsv_to_biom" >&2
-	#~ exit 1;
-#~ fi
-#~ 
-#~ echo "Step tree : pynast `date`"
-#~ 
-#~ tree.py \
- #~ --nb-cpus $nb_cpu  \
- #~ --input-otu $out_dir/04-filters.fasta \
- #~ --biomfile $out_dir/04-affiliation.biom \
- #~ --template-pynast $frogs_dir/test/data/otus_pynast.fasta \
- #~ --out-tree $out_dir/10a-tree.nwk \
- #~ --html $out_dir/10a-tree.html \
- #~ --log-file $out_dir/10a-tree.log
- #~ 
-#~ if [ $? -ne 0 ]
-#~ then
-	#~ echo "Error in tree : pynast" >&2
-	#~ exit 1;
-#~ fi
+clustering.py \
+ --distance 3 \
+ --denoising \
+ --input-fasta $out_dir/01-prepro.fasta \
+ --input-count $out_dir/01-prepro.tsv \
+ --output-biom $out_dir/02-clustering.biom \
+ --output-fasta $out_dir/02-clustering.fasta \
+ --output-compo $out_dir/02-clustering_compo.tsv \
+ --log-file $out_dir/02-clustering.log \
+ --nb-cpus $nb_cpu
+
+if [ $? -ne 0 ]
+then
+	echo "Error in clustering" >&2
+	exit 1;
+fi
+
+
+echo "Step remove_chimera `date`"
+
+remove_chimera.py \
+ --input-fasta $out_dir/02-clustering.fasta \
+ --input-biom $out_dir/02-clustering.biom \
+ --non-chimera $out_dir/03-chimera.fasta \
+ --out-abundance $out_dir/03-chimera.biom \
+ --summary $out_dir/03-chimera.html \
+ --log-file $out_dir/03-chimera.log \
+ --nb-cpus $nb_cpu
+ 
+if [ $? -ne 0 ]
+then
+	echo "Error in remove_chimera" >&2
+	exit 1;
+fi
+
+
+echo "Step filters `date`"
+
+filters.py \
+ --min-abundance 0.00005 \
+ --min-sample-presence 3\
+ --input-biom $out_dir/03-chimera.biom \
+ --input-fasta $out_dir/03-chimera.fasta \
+ --output-fasta $out_dir/04-filters.fasta \
+ --output-biom $out_dir/04-filters.biom \
+ --excluded $out_dir/04-filters.excluded \
+ --summary $out_dir/04-filters.html \
+ --log-file $out_dir/04-filters.log 
+
+if [ $? -ne 0 ]
+then
+	echo "Error in filters" >&2
+	exit 1;
+fi
+
+
+echo "Step affiliation_OTU `date`"
+
+affiliation_OTU.py \
+ --reference $frogs_dir/test/data/db.fasta \
+ --input-fasta $out_dir/04-filters.fasta \
+ --input-biom $out_dir/04-filters.biom \
+ --output-biom $out_dir/04-affiliation.biom \
+ --summary $out_dir/04-affiliation.html \
+ --log-file $out_dir/04-affiliation.log \
+ --nb-cpus $nb_cpu --java-mem $java_mem \
+ --rdp
+
+if [ $? -ne 0 ]
+then
+	echo "Error in affiliation_OTU" >&2
+	exit 1;
+fi
+
+
+echo "Step clusters_stat `date`"
+
+clusters_stat.py \
+ --input-biom $out_dir/04-affiliation.biom \
+ --output-file $out_dir/05-clustersStat.html \
+ --log-file $out_dir/05-clustersStat.log
+
+if [ $? -ne 0 ]
+then
+	echo "Error in clusters_stat" >&2
+	exit 1;
+fi
+
+
+echo "Step affiliations_stat `date`"
+
+affiliations_stat.py \
+ --input-biom $out_dir/04-affiliation.biom \
+ --output-file $out_dir/06-affiliationsStat.html \
+ --log-file $out_dir/06-affiliationsStat.log \
+ --tax-consensus-tag "blast_taxonomy" \
+ --identity-tag "perc_identity" \
+ --coverage-tag "perc_query_coverage" \
+ --multiple-tag "blast_affiliations" \
+ --rarefaction-ranks Family Genus Species
+
+if [ $? -ne 0 ]
+then
+	echo "Error in affiliations_stat" >&2
+	exit 1;
+fi
+
+
+echo "Step biom_to_tsv `date`"
+
+biom_to_tsv.py \
+ --input-biom $out_dir/04-affiliation.biom \
+ --input-fasta $out_dir/04-filters.fasta \
+ --output-tsv $out_dir/07-biom2tsv.tsv \
+ --output-multi-affi $out_dir/07-biom2tsv.multi \
+ --log-file $out_dir/07-biom2tsv.log
+
+if [ $? -ne 0 ]
+then
+	echo "Error in biom_to_tsv" >&2
+	exit 1;
+fi
+
+echo "Step biom_to_stdBiom `date`"
+
+
+biom_to_stdBiom.py \
+ --input-biom $out_dir/04-affiliation.biom \
+ --output-biom $out_dir/08-affiliation_std.biom \
+ --output-metadata $out_dir/08-affiliation_multihit.tsv \
+ --log-file $out_dir/08-biom2stdbiom.log
+
+if [ $? -ne 0 ]
+then
+	echo "Error in biom_to_stdBiom" >&2
+	exit 1;
+fi
+
+echo "Step tsv_to_biom `date`"
+
+
+tsv_to_biom.py \
+ --input-tsv $out_dir/07-biom2tsv.tsv \
+ --input-multi-affi $out_dir/07-biom2tsv.multi \
+ --output-biom $out_dir/09-tsv2biom.biom \
+ --output-fasta $out_dir/09-tsv2biom.fasta \
+ --log-file $out_dir/09-tsv2biom.log 
+
+if [ $? -ne 0 ]
+then
+	echo "Error in tsv_to_biom" >&2
+	exit 1;
+fi
+
+echo "Step tree : pynast `date`"
+
+tree.py \
+ --nb-cpus $nb_cpu  \
+ --input-otu $out_dir/04-filters.fasta \
+ --biomfile $out_dir/04-affiliation.biom \
+ --template-pynast $frogs_dir/test/data/otus_pynast.fasta \
+ --out-tree $out_dir/10a-tree.nwk \
+ --html $out_dir/10a-tree.html \
+ --log-file $out_dir/10a-tree.log
+ 
+if [ $? -ne 0 ]
+then
+	echo "Error in tree : pynast" >&2
+	exit 1;
+fi
 
 echo "Step tree : mafft `date`"
 
