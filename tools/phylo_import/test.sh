13c372a7191d647ed7498ea2cfc7bf6cba861976
mariabernard <maria.bernard@inra.fr>
2018-05-14 16:51:02 +0200

    FROGSSTAT Phyloseq import can take unstandardized FROGS biom as input

--- tools/phylo_import/test.sh
+++ tools/phylo_import/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-FROGS_DIR=`dirname $(dirname $(pwd))`
-export PATH=$FROGS_DIR/libexec:$PATH
-export PYTHONPATH=$FROGS_DIR/lib:$PYTHONPATH
+#~ FROGS_DIR=`dirname $(dirname $(pwd))`
+#~ export PATH=$FROGS_DIR/libexec:$FROGS_DIR/app:$PATH
+#~ export PYTHONPATH=$FROGS_DIR/lib:$PYTHONPATH
 
 # Create output folder
 if [ ! -d "test" ]
@@ -19,3 +19,10 @@ python r_import_data.py  \
 	-s data/sample_data.tsv \
 	-t data/tree.nwk \
 	--rdata test/phylo_import.Rdata -o test/phylo_import.html -l test/phylo_import.log
+
+# on unstandardized biom
+python r_import_data.py  \
+	-n \
+	-b data/frogs.biom \
+	-s data/frogs_sample_data.tsv \
+	--rdata test/frogs_import.Rdata -o test/frogs_import.html -l test/frogs_import.log
