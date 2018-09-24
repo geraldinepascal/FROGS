ab6ae763ac8e1770e9fdc8949dcdb65b07b38d9f
mariabernard <maria.bernard@inra.fr>
2018-09-24 17:03:01 +0200

    ITSx test formatting

--- tools/itsx/test.sh
+++ tools/itsx/test.sh
@@ -12,7 +12,11 @@ else
 fi
 
 echo "ITSx"
-./itsx.py --input-fasta data/input.fasta --input-biom data/input.biom --region ITS1 --out-abundance test/itsx.biom --summary test/itsx.html --log-file test/itsx.log --out-fasta test/itsx.fasta --out-removed test/removed.fasta
+./itsx.py --input-fasta data/input.fasta --input-biom data/input.biom \
+    --region ITS1 \
+    --out-abundance test/itsx.biom --summary test/itsx.html --log-file test/itsx.log --out-fasta test/itsx.fasta --out-removed test/removed.fasta
 
 echo "ITSx ckeck ITS only"
-./itsx.py --input-fasta data/input.fasta --input-biom data/input.biom --region ITS1 --out-abundance test/itsx.biom --summary test/itsx2.html --log-file test/itsx2.log --out-fasta test/itsx2.fasta --check-its-only --out-removed test/removed2.fasta 
+./itsx.py --input-fasta data/input.fasta --input-biom data/input.biom \
+    --region ITS1 --check-its-only \
+    --out-abundance test/itsx.biom --summary test/itsx2.html --log-file test/itsx2.log --out-fasta test/itsx2.fasta --out-removed test/removed2.fasta 
