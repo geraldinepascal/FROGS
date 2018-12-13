a77053c7772392c1ca3f8660a83c3892834c3376
mariabernard <maria.bernard@inra.fr>
2018-12-13 15:40:46 +0100

    Tree, remove pynast support and clean code

--- tools/tree/tree.py
+++ tools/tree/tree.py
@@ -47,50 +47,23 @@ from frogsBiom import BiomIO
 #
 ##################################################################################################################################################
 
-class Pynast(Cmd):
-    """
-    @summary: Multiple alignment of observed candidate sequences with aligned well-known sequences.
-    @see: https://github.com/biocore/pynast
-    """
-    def __init__(self, in_candi_pynast, in_templ_pynast, min_len, pynast_align, pynast_fail, pynast_log):
-        """
-        @param in_candi_pynast: [str] Path to input fasta file.
-        @param in_templ_pynast: [str] Path to template alignment file.
-        @param min_len: [int] Minimum sequence length to include in NAST alignment.
-        @param align: [str] Path to Pynast aligned sequences output.
-        @param pynast_fail: [str] Path to Pynast failed aligned sequences output.
-        @param pynast_log: [str] Path to Pynast log file.
-        """
-        Cmd.__init__( self,
-                      'pynast',
-                      'Pynast multiple alignment.',
-                      "--input_fp " + in_candi_pynast + " --template_fp " + in_templ_pynast + " --min_len " + min_len + " --pairwise_alignment_method mafft --fasta_out "+ pynast_align + " --failure_fp " + pynast_fail + " --log_fp " + pynast_log ,
-                      "--version")
-
-    def get_version(self):
-        """
-        @summary: Returns the program version number.
-        @return: [str] Version number if this is possible, otherwise this method return 'unknown'.
-        """
-        return Cmd.get_version(self, 'stdout').split()[2].strip()
-
 class Mafft(Cmd):
     """
     @summary: Mafft denovo multiple alignment of observed candidate sequences.
     @see: http://mafft.cbrc.jp/alignment/software/
     """
-    def __init__(self, mafftMet, fasta, aligned, thread, mafft_stderr):
+    def __init__(self, mafftMet, fasta, aligned, thread, stderr):
         """
         @param mafftMet: [str] Mafft method option.
         @param fasta: [str] Path to input fasta file.
         @param aligned: [str] Path to store resulting alignment file.
-        @param mafft_stderr: [str] Path to temporary Mafft stderr output file
+        @param stderr: [str] Path to temporary Mafft stderr output file
         @param thread: [int] number of cpu to use.
         """
         Cmd.__init__(self,
                  'mafft',
                  'Mafft multiple alignment.',
-                  "" + mafftMet + "--thread "+ str(thread) +" "+ fasta +" > "+aligned +' 2> ' + mafft_stderr,
+                  "" + mafftMet + "--thread "+ str(thread) +" "+ fasta +" > "+aligned +' 2> ' + stderr,
                   "--version")
 
     def get_version(self):
@@ -161,15 +134,6 @@ def get_fasta_nb_seq( fasta_file ):
     """
     return sum(1 for _ in (FastaIO(fasta_file)))
 
-def compute_min_sequence_length(seqs):
-    """
-    @summary:  return the minimum sequence length.
-    @param seqs : [str] Path to input fasta file.
-    """
-    FH_in=FastaIO(seqs)
-    min_length = min([len(record.string) for record in FH_in ])
-    return str(min_length)
-
 def get_methods_mafft(seqs):
     """
     @summary: return mafft options
@@ -186,39 +150,45 @@ def get_methods_mafft(seqs):
     else:
         return "--auto "
         
-def write_summary( summary_file, fasta_in, pynast_fail, biomfile, treefile):
+def write_summary( summary_file, fasta_in, align_out, biomfile, treefile):
     """
     @summary: Writes the process summary in one html file.
     @param summary_file: [str] path to the output html file.
-    @param pynast_fail: [str] path to the pynast_fail file.
+    @param align_out: [str] path to the fasta file of unaligned OTU
     @param biomfile: [str] path to the input BIOM file.
     @param treefile: [str] path to the Newick file.
     """
     # to summary OTUs number && abundances number               
-    detection_categories =["Taxonomic Information", "Abundance Number", "% with abundance total", "Sequence length"]
-    table_otu_out =[]
     summary_info = {
-       'number_otu_all' : 0,
        'otu_kept' : 0,
        'otu_removed' : 0,
-       'number_abundance_all' : 0,
        'abundance_kept' : 0,
        'abundance_removed' : 0       
     }
-    biom=BiomIO.from_json(biomfile)
+    number_otu_all = 0
+    number_abundance_all = 0
+    # to detail removed OTU
+    removed_details_categories =["Taxonomic Information", "Abundance Number", "% with abundance total", "Sequence length"]
+    removed_details_data =[]
+    
     # to build one metadata for tree view
     dic_otu={}
-    treefile = open(treefile, "r")
-    newick = treefile.read().strip()
     list_otu_all=list()
     list_out_tree=[]
+
+    biom=BiomIO.from_json(biomfile)
+    treefile = open(treefile, "r")
+    newick = treefile.read().strip()
+
+    # record nb OTU and abundance
     for otu in FastaIO(fasta_in):
         list_otu_all.append(otu.id)
-        summary_info['number_otu_all'] +=1
-        summary_info['number_abundance_all'] += biom.get_observation_count(otu.id)
+        number_otu_all +=1
+        number_abundance_all += biom.get_observation_count(otu.id)
 
-    if pynast_fail is not None:
-        for otu in FastaIO(pynast_fail):
+    # record details about removed OTU
+    if align_out is not None:
+        for otu in FastaIO(align_out):
             summary_info['otu_removed'] +=1
             summary_info['abundance_removed'] += biom.get_observation_count(otu.id)
             
@@ -229,14 +199,14 @@ def write_summary( summary_file, fasta_in, pynast_fail, biomfile, treefile):
             elif biom.has_metadata("blast_taxonomy"): 
                 taxonomy = ";".join(biom.get_observation_metadata(otu.id)["blast_taxonomy"]) if issubclass(biom.get_observation_metadata(otu.id)["blast_taxonomy"].__class__,list) else str(biom.get_observation_metadata(otu.id)["blast_taxonomy"])
             abundance=biom.get_observation_count(otu.id)
-            percent_abundance=abundance*100/(float(summary_info['number_abundance_all']))
+            percent_abundance=abundance*100/(float(number_abundance_all))
             length=len(otu.string)
             info={"name": otu.id, "data": [taxonomy, abundance, percent_abundance, length]}
-            table_otu_out.append(info)
+            removed_details_data.append(info)
             list_out_tree.append(otu.id)
 
-    list_in_tree=[item for item in list_otu_all if item not in list_out_tree]
-    
+    # improve tree view by adding taxonomy information
+    list_in_tree=[item for item in list_otu_all if item not in list_out_tree]  
     for otu in list_in_tree:
         tax=None
         if biom.has_metadata("taxonomy"):
@@ -245,23 +215,25 @@ def write_summary( summary_file, fasta_in, pynast_fail, biomfile, treefile):
             tax=" ".join(biom.get_observation_metadata(otu)["blast_taxonomy"]) if issubclass(biom.get_observation_metadata(otu)["blast_taxonomy"].__class__, list) else str(biom.get_observation_metadata(otu)["blast_taxonomy"])
         if tax :
             newick=newick.replace(otu + ":", otu + " " + tax + ":")
-            
-    summary_info['otu_kept'] = summary_info['number_otu_all'] - summary_info['otu_removed']
-    summary_info['abundance_kept'] = summary_info['number_abundance_all'] - summary_info['abundance_removed']
+    
+    # finalize summary
+    summary_info['otu_kept'] = number_otu_all - summary_info['otu_removed']
+    summary_info['abundance_kept'] = number_abundance_all - summary_info['abundance_removed']
+    
     # Write
-    FH_summary_tpl = open( os.path.join(CURRENT_DIR, "tree_tpl.html") )
+    FH_summary_tpl = open( os.path.join(CURRENT_DIR, "tree_tpl2.html") )
     FH_summary_out = open( summary_file, "w" )
     for line in FH_summary_tpl:
         if "###HEIGHT###" in line:
             line = line.replace( "###HEIGHT###", json.dumps(summary_info['otu_kept']*11+166))
         if "###NEWICK###" in line:
             line = line.replace( "###NEWICK###", newick)
-        if "###DETECTION_CATEGORIES###" in line:
-            line = line.replace( "###DETECTION_CATEGORIES###", json.dumps(detection_categories) )
-        elif "###DETECTION_DATA###" in line:
-            line = line.replace( "###DETECTION_DATA###", json.dumps(table_otu_out) )
-        elif "###REMOVE_DATA###" in line:
-            line = line.replace( "###REMOVE_DATA###", json.dumps(summary_info) )
+        if "##REMOVED_DETAILS_CATEGORIES###" in line:
+            line = line.replace( "###REMOVED_DETAILS_CATEGORIES###", json.dumps(removed_details_categories) )
+        elif "###REMOVED_DETAILS_DATA###" in line:
+            line = line.replace( "###REMOVED_DETAILS_DATA###", json.dumps(removed_details_data) )
+        elif "###SUMMARY###" in line:
+            line = line.replace( "###SUMMARY###", json.dumps(summary_info) )
         elif '<div id="OTUs-fail" style="display:none;">' in line:
             if summary_info['otu_removed']!=0:
                 line = line.replace( 'style="display:none;"', '' )
@@ -285,7 +257,6 @@ if __name__ == "__main__":
     # Inputs
     group_input = parser.add_argument_group( 'Inputs' )
     group_input.add_argument( '-i', '--input-otu', required=True, help='Path to input fasta file of OTU. Warning: FROGS Tree is only working on less than 10000 sequences!' )
-    group_input.add_argument( '-t', '--template-pynast', help='Path to a template alignment file if available for Pynast (format: fasta).' )
     group_input.add_argument( '-b', '--biomfile', help='Path to the abundance biom file.' )
         
     # output
@@ -296,18 +267,17 @@ if __name__ == "__main__":
     args = parser.parse_args()
     prevent_shell_injections(args)
     
-    # Temporary files
+    ### Temporary files
     tmpFiles=TmpFiles(os.path.split(args.out_tree)[0])
     filename_prefix = ".".join(os.path.split(args.input_otu)[1].split('.')[:-1])
-    if args.template_pynast is None:
-        mafft_stderr = tmpFiles.add("mafft.stderr")
-        align= tmpFiles.add('mafft_aligned.fasta')   
-        pynast_fail=None
-    else:
-        align = tmpFiles.add('pynast_aligned.fasta')
-        pynast_fail = tmpFiles.add('pynast_fail.fasta')
-        pynast_log = tmpFiles.add('pynast_log.txt') 
     
+    # alignment temporary files
+    stderr = tmpFiles.add("mafft.stderr")
+    align= tmpFiles.add('mafft_aligned.fasta')   
+    # if we want to add alignment method that do not keep necessarily all OTU, such as pynast when supported in FROGS
+    align_out=None 
+    
+    # fastree temporary files
     fasttree=tmpFiles.add("fasttree.nwk")
     fasttree_stderr=tmpFiles.add("fasttree.stderr")
     
@@ -321,15 +291,19 @@ if __name__ == "__main__":
         Logger.static_write(args.log_file, "Number of input OTUs sequences: " + str(nb_seq) + "\n\n")
         if nb_seq >10000:
             raise Exception( "FROGS Tree is only working on less than 10 000 sequences!" )
-        if args.template_pynast is None:
-            mafftMet=get_methods_mafft(args.input_otu)
-            Mafft(mafftMet, args.input_otu, align, args.nb_cpus, mafft_stderr).submit( args.log_file )
-        else:
-            min_len=compute_min_sequence_length(args.input_otu)
-            Pynast(args.input_otu, args.template_pynast, min_len, align, pynast_fail, pynast_log).submit( args.log_file )
+        
+        # alignment step
+        mafftMet=get_methods_mafft(args.input_otu)
+        Mafft(mafftMet, args.input_otu, align, args.nb_cpus, stderr).submit( args.log_file )
+
+        # tree contruction step
         FastTree(align, fasttree, fasttree_stderr).submit( args.log_file )
+
+        # rooting tree step
         RootTree(fasttree, args.out_tree).submit(args.log_file)
-        write_summary( args.html, args.input_otu, pynast_fail, args.biomfile, args.out_tree)
+
+        # summarize resultats in HTML output 
+        write_summary( args.html, args.input_otu, align_out, args.biomfile, args.out_tree)
     finally:
         if not args.debug:
             tmpFiles.deleteAll()
