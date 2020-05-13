136a3a62c2e823ca697e79035e4755c110660f1d
mariabernard <maria.bernard@inra.fr>
2020-05-13 09:18:24 +0200

    update the test.sh script

--- test/test.sh
+++ test/test.sh
@@ -34,7 +34,7 @@ preprocess.py illumina \
  --output-dereplicated $out_dir/01-prepro-flash.fasta \
  --output-count $out_dir/01-prepro-flash.tsv \
  --summary $out_dir/01-prepro-flash.html \
- --log-file $out_dir/01-prepro-flash.log 
+ --log-file $out_dir/01-prepro-flash.log
  
 if [ $? -ne 0 ]
 then
@@ -99,9 +99,9 @@ then
 fi
 
 
-echo "Step filters `date`"
+echo "Step otu filters `date`"
 
-filters.py \
+otu-filters.py \
  --min-abundance 0.00005 \
  --min-sample-presence 3 \
  --nb-cpus $nb_cpu \
@@ -155,16 +155,68 @@ then
 	exit 1;
 fi
 
+
+echo "Step affiliation_filter: masking mode `date`"
+
+affiliation_filters.py \
+--input-biom $out_dir/06-affiliation.biom \
+--input-fasta $out_dir/04-filters.fasta \
+--output-biom $out_dir/07-affiliation_masked.biom \
+--summary $out_dir/07-affiliation_masked.html \
+--impacted $out_dir/07-impacted_OTU_masked.tsv \
+--log-file $out_dir/07-affiliation_filter_maskMode.log \
+--min-rdp-bootstrap Species:0.8 \
+--min-blast-length 150 \
+--min-blast-identity 1.0 \
+--min-blast-coverage 1.0 \
+--max-blast-evalue 1e-150 \
+--taxon-ignored "g__Sarcodon" "s__Trichoderma" \
+--mask \
+--taxonomic-ranks Domain Phylum Class Order Family Genus Species  # this is the default value of this option
+
+if [ $? -ne 0 ]
+then
+	echo "Error in affiliation_filter" >&2
+	exit 1;
+fi
+
+
+echo "Step affiliation_filter: deleted mode `date`"
+
+affiliation_filters.py \
+--input-biom $out_dir/06-affiliation.biom \
+--input-fasta $out_dir/04-filters.fasta \
+--output-biom $out_dir/07-affiliation_deleted.biom \
+--output-fasta $out_dir/07-affiliation_deleted.fasta \
+--summary $out_dir/07-affiliation_deleted.html \
+--impacted $out_dir/07-impacted_OTU_deleted.tsv \
+--log-file $out_dir/07-affiliation_filter_delMode.log \
+--min-rdp-bootstrap Species:0.8 \
+--min-blast-length 150 \
+--min-blast-identity 1.0 \
+--min-blast-coverage 1.0 \
+--max-blast-evalue 1e-150 \
+--taxon-ignored "g__Sarcodon" "s__Trichoderma" \
+--delete \
+--taxonomic-ranks Domain Phylum Class Order Family Genus Species  # this is the default value of this option
+
+if [ $? -ne 0 ]
+then
+	echo "Error in affiliation_filter" >&2
+	exit 1;
+fi
+
+
 echo "Step affiliation_postprocess `date`"
 
 affiliation_postprocess.py \
  --input-biom $out_dir/06-affiliation.biom \
  --input-fasta $out_dir/04-filters.fasta \
  --reference $frogs_dir/test/data/Unite_extract_ITS1.fasta \
- --output-biom $out_dir/07-affiliation_postprocessed.biom \
- --output-compo $out_dir/07-affiliation_postprocessed.compo.tsv \
- --output-fasta $out_dir/07-affiliation_postprocessed.fasta \
- --log-file $out_dir/07-affiliation_postprocessed.log
+ --output-biom $out_dir/08-affiliation_postprocessed.biom \
+ --output-compo $out_dir/08-affiliation_postprocessed.compo.tsv \
+ --output-fasta $out_dir/08-affiliation_postprocessed.fasta \
+ --log-file $out_dir/08-affiliation_postprocessed.log
 
 if [ $? -ne 0 ]
 then
@@ -176,9 +228,9 @@ fi
 echo "Step clusters_stat `date`"
 
 clusters_stat.py \
- --input-biom $out_dir/06-affiliation.biom \
- --output-file $out_dir/08-clustersStat.html \
- --log-file $out_dir/08-clustersStat.log
+ --input-biom $out_dir/07-affiliation_masked.biom \
+ --output-file $out_dir/09-clustersStat.html \
+ --log-file $out_dir/09-clustersStat.log
 
 if [ $? -ne 0 ]
 then
@@ -186,18 +238,19 @@ then
 	exit 1;
 fi
 
-
+					
 echo "Step affiliations_stat `date`"
 
 affiliations_stat.py \
- --input-biom $out_dir/06-affiliation.biom \
- --output-file $out_dir/09-affiliationsStat.html \
- --log-file $out_dir/09-affiliationsStat.log \
+ --input-biom $out_dir/07-affiliation_masked.biom \
+ --output-file $out_dir/10-affiliationsStat.html \
+ --log-file $out_dir/10-affiliationsStat.log \
  --tax-consensus-tag "blast_taxonomy" \
  --identity-tag "perc_identity" \
  --coverage-tag "perc_query_coverage" \
  --multiple-tag "blast_affiliations" \
- --rarefaction-ranks Family Genus Species
+ --rarefaction-ranks Family Genus Species \
+ --taxonomic-ranks Domain Phylum Class Order Family Genus Species # this is the default value of this option
 
 if [ $? -ne 0 ]
 then
@@ -209,11 +262,11 @@ fi
 echo "Step biom_to_tsv `date`"
 
 biom_to_tsv.py \
- --input-biom $out_dir/06-affiliation.biom \
+ --input-biom $out_dir/07-affiliation_masked.biom \
  --input-fasta $out_dir/04-filters.fasta \
- --output-tsv $out_dir/10-biom2tsv.tsv \
- --output-multi-affi $out_dir/10-biom2tsv-affiliation_multihit.tsv \
- --log-file $out_dir/10-biom2tsv.log
+ --output-tsv $out_dir/11-biom2tsv.tsv \
+ --output-multi-affi $out_dir/11-biom2tsv-affiliation_multihit.tsv \
+ --log-file $out_dir/11-biom2tsv.log
 
 if [ $? -ne 0 ]
 then
@@ -225,10 +278,10 @@ echo "Step biom_to_stdBiom `date`"
 
 
 biom_to_stdBiom.py \
- --input-biom $out_dir/06-affiliation.biom \
- --output-biom $out_dir/11-affiliation_std.biom \
- --output-metadata $out_dir/11-affiliation_multihit.tsv \
- --log-file $out_dir/11-biom2stdbiom.log
+ --input-biom $out_dir/07-affiliation_masked.biom \
+ --output-biom $out_dir/12-affiliation_std.biom \
+ --output-metadata $out_dir/12-affiliation_multihit.tsv \
+ --log-file $out_dir/12-biom2stdbiom.log
 
 if [ $? -ne 0 ]
 then
@@ -240,11 +293,11 @@ echo "Step tsv_to_biom `date`"
 
 
 tsv_to_biom.py \
- --input-tsv $out_dir/10-biom2tsv.tsv \
- --input-multi-affi $out_dir/10-biom2tsv-affiliation_multihit.tsv \
- --output-biom $out_dir/12-tsv2biom.biom \
- --output-fasta $out_dir/12-tsv2biom.fasta \
- --log-file $out_dir/12-tsv2biom.log 
+ --input-tsv $out_dir/11-biom2tsv.tsv \
+ --input-multi-affi $out_dir/11-biom2tsv-affiliation_multihit.tsv \
+ --output-biom $out_dir/13-tsv2biom.biom \
+ --output-fasta $out_dir/13-tsv2biom.fasta \
+ --log-file $out_dir/13-tsv2biom.log 
 
 if [ $? -ne 0 ]
 then
@@ -257,24 +310,25 @@ echo "Step tree : mafft `date`"
 tree.py \
  --nb-cpus $nb_cpu \
  --input-otu $out_dir/04-filters.fasta \
- --biomfile $out_dir/06-affiliation.biom \
- --out-tree $out_dir/13-tree-mafft.nwk \
- --html $out_dir/13-tree-mafft.html \
- --log-file $out_dir/13-tree-mafft.log
+ --biomfile $out_dir/07-affiliation_masked.biom \
+ --out-tree $out_dir/14-tree-mafft.nwk \
+ --html $out_dir/14-tree-mafft.html \
+ --log-file $out_dir/14-tree-mafft.log
 
 if [ $? -ne 0 ]
 then
 	echo "Error in tree : mafft" >&2
 	exit 1;
 fi
-
 echo "Step r_import_data `date`"
 
 r_import_data.py  \
  --biomfile $frogs_dir/test/data/chaillou.biom \
  --samplefile $frogs_dir/test/data/sample_metadata.tsv \
  --treefile $frogs_dir/test/data/tree.nwk \
- --rdata $out_dir/14-phylo_import.Rdata --html $out_dir/14-phylo_import.html --log-file $out_dir/14-phylo_import.log
+ --rdata $out_dir/15-phylo_import.Rdata \
+ --html $out_dir/15-phylo_import.html \
+ --log-file $out_dir/15-phylo_import.log
 
  
 if [ $? -ne 0 ]
@@ -287,8 +341,9 @@ echo "Step r_composition `date`"
 
 r_composition.py  \
  --varExp EnvType --taxaRank1 Kingdom --taxaSet1 Bacteria --taxaRank2 Phylum --numberOfTaxa 9 \
- --rdata $out_dir/14-phylo_import.Rdata \
- --html $out_dir/15-phylo_composition.html --log-file $out_dir/15-phylo_composition.log
+ --rdata $out_dir/15-phylo_import.Rdata \
+ --html $out_dir/16-phylo_composition.html \
+ --log-file $out_dir/16-phylo_composition.log
 
  
 if [ $? -ne 0 ]
@@ -301,8 +356,10 @@ echo "Step r_alpha_diversity `date`"
 
 r_alpha_diversity.py  \
  --varExp EnvType \
- --rdata $out_dir/14-phylo_import.Rdata --alpha-measures Observed Chao1 Shannon \
- --alpha-out $out_dir/16-phylo_alpha_div.tsv --html $out_dir/16-phylo_alpha_div.html --log-file $out_dir/16-phylo_alpha_div.log
+ --rdata $out_dir/15-phylo_import.Rdata --alpha-measures Observed Chao1 Shannon \
+ --alpha-out $out_dir/17-phylo_alpha_div.tsv \
+ --html $out_dir/17-phylo_alpha_div.html \
+ --log-file $out_dir/17-phylo_alpha_div.log
 
  
 if [ $? -ne 0 ]
@@ -315,8 +372,10 @@ echo "Step r_beta_diversity `date`"
 
 r_beta_diversity.py  \
  --varExp EnvType --distance-methods cc,unifrac \
- --rdata $out_dir/14-phylo_import.Rdata \
- --matrix-outdir $out_dir --html $out_dir/17-phylo_beta_div.html --log-file $out_dir/17-phylo_beta_div.log
+ --rdata $out_dir/15-phylo_import.Rdata \
+ --matrix-outdir $out_dir \
+ --html $out_dir/18-phylo_beta_div.html \
+ --log-file $out_dir/18-phylo_beta_div.log
 
  
 if [ $? -ne 0 ]
@@ -329,8 +388,9 @@ echo "Step r_structure `date`"
 
 r_structure.py  \
  --varExp EnvType --ordination-method MDS \
- --rdata $out_dir/14-phylo_import.Rdata --distance-matrix $out_dir/Unifrac.tsv \
- --html $out_dir/18-phylo_structure.html --log-file $out_dir/18-phylo_structure.log
+ --rdata $out_dir/15-phylo_import.Rdata --distance-matrix $out_dir/unifrac.tsv \
+ --html $out_dir/19-phylo_structure.html \
+ --log-file $out_dir/19-phylo_structure.log
 
  
 if [ $? -ne 0 ]
@@ -343,8 +403,9 @@ echo "Step r_clustering `date`"
 
 r_clustering.py  \
  --varExp EnvType \
- --rdata $out_dir/14-phylo_import.Rdata --distance-matrix $out_dir/Unifrac.tsv \
- --html $out_dir/19-phylo_clutering.html --log-file $out_dir/19-phylo_clustering.log
+ --rdata $out_dir/15-phylo_import.Rdata --distance-matrix $out_dir/unifrac.tsv \
+ --html $out_dir/20-phylo_clutering.html \
+ --log-file $out_dir/20-phylo_clustering.log
 
  
 if [ $? -ne 0 ]
@@ -357,14 +418,47 @@ echo "Step r_manova `date`"
 
 r_manova.py  \
  --varExp EnvType \
- --rdata $out_dir/14-phylo_import.Rdata --distance-matrix $out_dir/Unifrac.tsv \
- --html $out_dir/20-phylo_manova.html --log-file $out_dir/20-phylo_manova.log
+ --rdata $out_dir/15-phylo_import.Rdata --distance-matrix $out_dir/unifrac.tsv \
+ --html $out_dir/21-phylo_manova.html \
+ --log-file $out_dir/21-phylo_manova.log
 
- 
 if [ $? -ne 0 ]
 then
 	echo "Error in r_manova " >&2
 	exit 1;
 fi
 
+
+echo "Step deseq2_preprocess `date`"
+
+deseq2_preprocess.py \
+ --data $out_dir/15-phylo_import.Rdata \
+ --log-file $out_dir/22-deseq2_preprocess.log \
+ --out-Rdata $out_dir/22-deseq2_preprocess.Rdata \
+ --var EnvType
+
+if [ $? -ne 0 ]
+then
+	echo "Error in deseq2_preprocess " >&2
+	exit 1;
+fi
+
+
+echo "Step deseq2_visualization `date`"
+
+deseq2_visualization.py \
+ --phyloseqData $out_dir/15-phylo_import.Rdata \
+ --dds $out_dir/22-deseq2_preprocess.Rdata \
+ --log-file $out_dir/23-deseq2_visualization.log \
+ --html $out_dir/22-deseq2_visualization.html \
+ --var EnvType --mod1 BoeufHache --mod2 SaumonFume
+                            
+
+if [ $? -ne 0 ]
+then
+	echo "Error in deseq2_visualization " >&2
+	exit 1;
+fi
+
 echo "Completed with success"
+
