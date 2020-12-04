60f1c2f70597cce090d8f61b775d289baf14483b
mariabernard <maria.bernard@inra.fr>
2020-12-04 14:32:22 +0100

    FROGS affi filters : update test.sh

--- tools/affiliation_filters/test.sh
+++ tools/affiliation_filters/test.sh
@@ -11,7 +11,6 @@ else
 	rm -r test
 fi
 
-
 # FROGS BIOM after affiliation
 echo ""
 OUT=test/metrics-filter
@@ -30,7 +29,9 @@ mkdir -p $OUT
 --delete
 
 echo ""
+OUT=test/metrics-filter
 echo $OUT "masking mode"
+mkdir -p $OUT
 
 ./affiliation_filters.py \
 --input-biom data/fake_affiliation.biom --input-fasta data/fake_affiliation.fasta \
@@ -44,6 +45,7 @@ echo $OUT "masking mode"
 --max-blast-evalue 0 \
 --mask
 
+
 echo ""
 OUT=test/taxon-ignored-filter
 echo $OUT "delete mode"
@@ -54,7 +56,7 @@ mkdir -p $OUT
 --output-biom $OUT/filtered_OTU_deleted.biom --output-fasta $OUT/filtered_OTU_deleted.fasta \
 --summary $OUT/summary_OTU_deleted.html --impacted $OUT/impacted_OTU_deleted.tsv --impacted-multihit $OUT/impacted_OTU_deleted_multihit.tsv \
 --log-file $OUT/delete_log.txt \
---taxon-ignored "Methylovulum miyakonense" "subsp." "unknown species" \
+--ignore-blast-taxa "Methylovulum miyakonense" "subsp." "unknown species" \
 --delete
 
 echo ""
@@ -65,11 +67,28 @@ echo $OUT "masking mode"
 --output-biom $OUT/filtered_OTU_masked.biom --output-fasta $OUT/filtered_OTU_masked.fasta \
 --summary $OUT/summary_OTU_masked.html --impacted $OUT/impacted_OTU_masked.tsv --impacted-multihit $OUT/impacted_OTU_masked_multihit.tsv \
 --log-file $OUT/mask_log.txt \
---taxon-ignored "Methylovulum miyakonense" "subsp." "unknown species" \
+--ignore-blast-taxa "Methylovulum miyakonense" "subsp." "unknown species" \
 --mask
 
 OUT=test/all-filter
 mkdir -p $OUT
+echo ""
+echo $OUT "deleting mode"
+
+./affiliation_filters.py \
+--input-biom data/fake_affiliation.biom --input-fasta data/fake_affiliation.fasta \
+--output-biom $OUT/filtered_OTU_deleted.biom --output-fasta $OUT/filtered_OTU_deleted.fasta \
+--summary $OUT/summary_OTU_deleted.html --impacted $OUT/impacted_OTU_deleted.tsv --impacted-multihit $OUT/impacted_OTU_deleted_multihit.tsv \
+--log-file $OUT/del_log.txt \
+--delete \
+--taxonomic-ranks Domain Phylum Class Order Family Genus Species \
+--min-rdp-bootstrap Species:0.8 \
+--min-blast-length 402 \
+--min-blast-identity 1.0 \
+--min-blast-coverage 1.0 \
+--max-blast-evalue 0 \
+--ignore-blast-taxa "Methylovulum miyakonense" "subsp." "unknown species"
+
 echo ""
 echo $OUT "masking mode"
 
@@ -79,6 +98,11 @@ echo $OUT "masking mode"
 --summary $OUT/summary_OTU_masked.html --impacted $OUT/impacted_OTU_masked.tsv --impacted-multihit $OUT/impacted_OTU_masked_multihit.tsv \
 --log-file $OUT/mask_log.txt \
 --mask \
---taxonomic-ranks Domain Phylum Class Order Family Genus Species --min-rdp-bootstrap Species:0.7303 \
---max-blast-evalue 0 --min-blast-identity 1.0 --min-blast-coverage 1.0 \
---taxon-ignored "Methylovulum miyakonense" "subsp." "unknown species" --debug
+--taxonomic-ranks Domain Phylum Class Order Family Genus Species \
+--min-rdp-bootstrap Species:0.8 \
+--min-blast-length 402 \
+--min-blast-identity 1.0 \
+--min-blast-coverage 1.0 \
+--max-blast-evalue 0 \
+--ignore-blast-taxa "Methylovulum miyakonense" "subsp." "unknown species"
+
