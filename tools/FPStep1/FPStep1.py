c08d7aa7d6f77ed97437402735ec087d5d017f50
Vincent Darbot <vincent.darbot@inrae.fr>
2021-04-29 23:38:07 +0200

    FPStep1: add write_summary

--- tools/FPStep1/FPStep1.py
+++ tools/FPStep1/FPStep1.py
@@ -12,7 +12,7 @@ import sys
 import argparse
 import json
 import re
-from numpy import median
+import ete3 as ete
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 # PATH
@@ -78,6 +78,103 @@ def convert_fasta(in_file, out_file):
 		FH_output.write(record)
 	FH_output.close()
 
+def excluded_sequence(file_tree, fasta_file, out_file):
+	"""
+	@summary: Returns the excluded sequence.
+	@param fasta_file: [str] Path to the fasta file to process.
+	@param tree_file: [str] Path to the tree file to process.
+	@return: [int] The file of no aligned sequence.
+	"""
+	file = open(file_tree, "r")
+	line = file.readline()
+	list_cluster = re.findall("(Cluster_[0-9]+)", line)
+	file.close()
+
+	FH_input = FastaIO(fasta_file)
+	FH_output = FastaIO(out_file, "wt")
+
+	for record in FH_input:
+		if record.id not in list_cluster:
+			FH_output.write(record)
+	FH_input.close()
+	FH_output.close()
+
+def write_summary(summary_file, fasta_in, align_out, biomfile, treefile):
+	"""
+	@summary: Writes the process summary in one html file.
+	@param summary_file: [str] path to the output html file.
+	@param align_out: [str] path to the fasta file of unaligned OTU
+	@param biomfile: [str] path to the input BIOM file.
+	@param treefile: [str] path to the Newick file.
+	"""
+	# to summary OTUs number && abundances number			   
+	summary_info = {
+	   'otu_kept' : 0,
+	   'otu_removed' : 0,
+	   'abundance_kept' : 0,
+	   'abundance_removed' : 0	   
+	}
+	number_otu_all = 0
+	number_abundance_all = 0
+	# to detail removed OTU
+	removed_details_categories =["Taxonomic Information", "Abundance Number", "% with abundance total", "Sequence length"]
+	removed_details_data =[]
+	
+	# to build one metadata for tree view
+	dic_otu={}
+	list_otu_all=list()
+	list_out_tree=[]
+
+	biom=BiomIO.from_json(biomfile)
+	treefile = open(treefile, "r")
+	newick = treefile.read().strip()
+
+	# record nb OTU and abundance
+	for otu in FastaIO(fasta_in):
+		list_otu_all.append(otu.id)
+		number_otu_all +=1
+		number_abundance_all += biom.get_observation_count(otu.id)
+
+	# record details about removed OTU
+	if align_out is not None:
+		for otu in FastaIO(align_out):
+			summary_info['otu_removed'] +=1
+			summary_info['abundance_removed'] += biom.get_observation_count(otu.id)
+			
+			# to built one table of OTUs out of phylogenetic tree
+			taxonomy=""
+			if biom.has_metadata("taxonomy"):
+				taxonomy = ";".join(biom.get_observation_metadata(otu.id)["taxonomy"]) if issubclass(biom.get_observation_metadata(otu.id)["taxonomy"].__class__,list) else str(biom.get_observation_metadata(otu.id)["taxonomy"])
+			elif biom.has_metadata("blast_taxonomy"): 
+				taxonomy = ";".join(biom.get_observation_metadata(otu.id)["blast_taxonomy"]) if issubclass(biom.get_observation_metadata(otu.id)["blast_taxonomy"].__class__,list) else str(biom.get_observation_metadata(otu.id)["blast_taxonomy"])
+			abundance=biom.get_observation_count(otu.id)
+			percent_abundance=abundance*100/(float(number_abundance_all))
+			length=len(otu.string)
+			info={"name": otu.id, "data": [taxonomy, abundance, percent_abundance, length]}
+			removed_details_data.append(info)
+			list_out_tree.append(otu.id)
+
+
+
+	summary_info['otu_kept'] = number_otu_all - summary_info['otu_removed']
+	summary_info['abundance_kept'] = number_abundance_all - summary_info['abundance_removed']
+
+
+	FH_summary_tpl = open( os.path.join(CURRENT_DIR, "remove_chimera_tpl.html") )
+	FH_summary_out = open( summary_file, "wt" )
+
+	for line in FH_summary_tpl:
+		if "###DETECTION_CATEGORIES###" in line:
+			line = line.replace( "###DETECTION_CATEGORIES###", json.dumps(removed_details_categories) )
+		elif "###DETECTION_DATA###" in line:
+			line = line.replace( "###DETECTION_DATA###", json.dumps(removed_details_data) )
+		elif "###REMOVE_DATA###" in line:
+			line = line.replace( "###REMOVE_DATA###", json.dumps(removed_details_data) )
+		FH_summary_out.write( line )
+
+	FH_summary_out.close()
+	FH_summary_tpl.close()
+
 
 ##################################################################################################################################################
 #
@@ -91,33 +188,36 @@ if __name__ == "__main__":
 	parser.add_argument( '--debug', default=False, action='store_true', help="Keep temporary files to debug program." )
 	# Inputs
 	group_input = parser.add_argument_group('Inputs')
-	group_input.add_argument('-s', '--study_fasta', required=True,
-	 help="Input fasta file of unaligned study sequences")
-	group_input.add_argument('-r', '--ref_dir', default=None,
-     help='Directory containing reference sequence files')
-	group_input.add_argument('-t','--placement_tool', default='epa-ng',
-	 help='Placement tool to use when placing sequences into reference tree. One of "epa-ng" or "sepp" must be input')
-    # Outputs
+	group_input.add_argument('-s', '--study_fasta', required=True, help="Input fasta file of unaligned study sequences")
+	group_input.add_argument('-b', '--biom_file', required=True, help='Biom file.')
+	group_input.add_argument('-r', '--ref_dir', default=None, help='Directory containing reference sequence files')
+	group_input.add_argument('-t','--placement_tool', default='epa-ng', help='Placement tool to use when placing sequences into reference tree. One of "epa-ng" or "sepp" must be input')
+	# Outputs
 	group_output = parser.add_argument_group('Outputs')
-	group_output.add_argument('-o', '--out_tree', default='out.tree',
-	 help='Normalized sequences (format: FASTA).')
-	group_output.add_argument('-l', '--log-file', default=sys.stdout,
-	 help='The list of commands executed.')
+	group_output.add_argument('-o', '--out_tree', default='out.tree', help='Normalized sequences (format: FASTA).')
+	group_output.add_argument('-l', '--log-file', default=sys.stdout, help='The list of commands executed.')
+	group_output.add_argument('-m','--html', default='summary.html', help="Path to store resulting html file. [Default: %(default)s]" )
 	args = parser.parse_args()
 	prevent_shell_injections(args)
 
-	tmpFiles=TmpFiles(os.path.split(args.out_tree)[0])
+	tmp_files=TmpFiles(os.path.split(args.out_tree)[0])
 
 	try:
+		Logger.static_write(args.log_file, "## Application\nSoftware :" + sys.argv[0] + " (version : " + str(__version__) + ")\nCommand : " + " ".join(sys.argv) + "\n\n")
 
-		fasta = tmpFiles.add('sout.fasta')
-		convert_fasta(args.study_fasta,fasta)
+		tmp_fasta = tmp_files.add('sout.fasta')
+		convert_fasta(args.study_fasta,tmp_fasta)
 
 		try:
 			PlaceSeqs(args.study_fasta, args.out_tree, args.placement_tool, args.ref_dir).submit(args.log_file)
 
 		except subprocess.CalledProcessError:
 			print('\n\n#ERROR : epa-ng running out of memory. Please use placement tool sepp instead ( -t sepp )')
+
+		excluded_sequence(args.out_tree,args.study_fasta,'excluded.fasta')
+
+		write_summary(args.html, tmp_fasta, 'excluded.fasta', args.biom_file, args.out_tree)
+
 	finally:
 		if not args.debug:
 			tmp_files.deleteAll()
