07b43558993abf669d8334410cd283c18f0ed12e
vindarbot <vincent.darbot@inrae.fr>
2021-12-07 15:24:40 +0100

    add FPStep4 test.sh

---
+++ tools/FPStep4/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+FROGS_DIR=`dirname $(dirname $(pwd))`
+export PATH=$FROGS_DIR/libexec:$PATH
+export PYTHONPATH=$FROGS_DIR/lib:$PYTHONPATH
+export GENE_HIERARCHY_FILE=$FROGS_DIR/default_files/gene_family_hierarchy.tsv
+
+# Create output folder
+if [ ! -d "test" ]
+then
+    mkdir test
+fi
+
+./FPStep4.py \
+ -i data/FPstep3_pred_abund.tsv \
+ -o test/test_FPStep4_pathways_abund.tsv \
+ -l test/test_FPStep4.log \
+ -t test/test_FPStep4.html
