7542c026ada516bac7880400360b65c726a4cfb6
vindarbot <vincent.darbot@inrae.fr>
2022-11-08 08:59:50 +0100

    affiliation_filters/affiliation_filters.py: add keep-blast-taxon , reverse of ingore-blast-taxon

--- tools/affiliation_filters/affiliation_filters.py
+++ tools/affiliation_filters/affiliation_filters.py
@@ -144,6 +144,15 @@ def ratioParameter( arg_value ):
         raise_exception( argparse.ArgumentTypeError("\n\n#ERROR : must be between 0.0 and 1.0.\n"))
     return float_arg_value
 
+def checkBlastTaxa( param ):
+    if param is not None:
+        temp = [ i.strip() for i in param if i.strip() != '']
+        if len(temp) == 0:
+            raise_exception( argparse.ArgumentTypeError( "\n\n#ERROR : keep-ignore-blast-taxa or ignore-blast-taxa list need to be non empty strings.\n\n" ))
+        else:
+            if len(temp) != len(param):
+                param = temp
+                Logger.static_write(args.log_file, "WARNING : empty string in keep-ignore-blast-taxa or ignore-blast-taxa option have been removed, here is the updated list that will be take into account: \"" + '\" \"'.join(args.ignore_blast_taxa) + "\n\n")
 
 def impacted_obs_on_rdpBootstrap(observation, taxonomic_depth, min_bootstrap):
     """
@@ -183,21 +192,24 @@ def impacted_blast_affi_on_blastMetrics( observation, tag, cmp_operator, thresho
 
     return blast_affiliations_out
 
-def impacted_blast_affi_on_blastTaxonomy(observation, taxon_ignored):
+def impacted_blast_affi_on_blastTaxonomy(observation, taxon_ignored, init_keep):
     """
     @summary: return blast affiliations whithout undesired taxon
     @param observation [obj] : observation object with list of blast affiliations
     @param taxon_ignored [list] : list of taxon to ignored (it may be partial terms)
     @return blast affiliations filtered list
     """
-
     blast_affiliations_out = dict()
     for idx,blast_affi in enumerate(observation['metadata']['blast_affiliations'] ):
-        keep=True
+        # True if ignore_blast_taxa, False if keep_blast_taxa
+        keep = init_keep
         for t in taxon_ignored:
             regexp = re.compile(t)
             if regexp.search(";".join(blast_affi["taxonomy"])):
-                keep=False
+                if keep:
+                    keep=False
+                else:
+                    keep=True
         if keep:
             blast_affiliations_out[idx] = blast_affi
 
@@ -411,9 +423,16 @@ def filter_biom(in_biom_file, impacted_file, output_file, params):
                     out_blast_affiliations.pop('filter_on_coverage')
 
             # add blast taxon to ignore criteria
-            if params.ignore_blast_taxa:
-                label = "Blast taxonomies belong to undesired taxon: " + " / ".join(args.ignore_blast_taxa)
-                out_blast_affiliations['filter_on_taxonIgnored'] = impacted_blast_affi_on_blastTaxonomy(observation, params.ignore_blast_taxa)
+            if params.ignore_blast_taxa or params.keep_blast_taxa:
+                if params.ignore_blast_taxa:
+                    param = params.ignore_blast_taxa
+                    keep = True
+                    label = "Blast taxonomies belong to undesired taxon: " + " / ".join(param)
+                else:
+                    param = params.keep_blast_taxa
+                    keep = False
+                    label = "Blast taxonomies belong to desired taxon: " + " / ".join(param)
+                out_blast_affiliations['filter_on_taxonIgnored'] = impacted_blast_affi_on_blastTaxonomy(observation, param, keep)
                 uniq_tax = get_uniq_tax(out_blast_affiliations['filter_on_taxonIgnored'])
                 if len(uniq_tax) != len(tax_in):
                     observation['metadata']['comment'].append("undesired_tax_in_blast")
@@ -423,7 +442,7 @@ def filter_biom(in_biom_file, impacted_file, output_file, params):
                 elif len(uniq_tax) == len(tax_in):
                     out_blast_affiliations.pop('filter_on_taxonIgnored')
 
-        elif params.min_blast_length or params.max_blast_evalue or params.min_blast_identity or params.min_blast_coverage or params.ignore_blast_taxa and not observation['metadata']['blast_affiliations']:
+        elif params.min_blast_length or params.max_blast_evalue or params.min_blast_identity or params.min_blast_coverage or params.ignore_blast_taxa or params.keep_blast_taxa and not observation['metadata']['blast_affiliations']:
             label = "Blast missing affiliations"
             if not label in impacted_dict:
                 impacted_dict[label] = list()
@@ -752,7 +771,9 @@ if __name__ == '__main__':
     group_exclusion_filter_bh.add_argument('-d','--delete', default=False, action='store_true', help="If affiliations do not respect one of the filter the entire OTU is deleted.(mutually exclusive with --mask)")
     #     Filters
     group_filter = parser.add_argument_group( 'Filters' )
-    group_filter.add_argument( '--ignore-blast-taxa', type=str, nargs='*', help="Taxon list to maks/delete in Blast affiliations")
+    group_filter_blast_taxa = group_filter.add_mutually_exclusive_group()
+    group_filter_blast_taxa.add_argument( '--ignore-blast-taxa', type=str, nargs='*', help="Taxon list to masks/delete in Blast affiliations")
+    group_filter_blast_taxa.add_argument( '--keep-blast-taxa', type=str, nargs='*', help="Taxon list to keep in Blast affiliations. All others affiliations will be masks/delete.")
     group_filter.add_argument( '-b', '--min-rdp-bootstrap', type=str, action=BootstrapParameter, metavar=("TAXONOMIC_LEVEL:MIN_BOOTSTRAP"), help="The minimal RDP bootstrap must be superior to this value (between 0 and 1)." )
     group_filter.add_argument( '-i', '--min-blast-identity', type=ratioParameter, help="The number corresponding to the blast percentage identity (between 0 and 1)." )
     group_filter.add_argument( '-c', '--min-blast-coverage', type=ratioParameter, help="The number corresponding to the blast percentage coverage (between 0 and 1)." )
@@ -788,15 +809,17 @@ if __name__ == '__main__':
             cmd.append(arg)
 
     Logger.static_write(args.log_file, "## Application\nSoftware: " + os.path.basename(sys.argv[0]) + " (version: " + str(__version__) + ")\nCommand: " + " ".join(cmd) + "\n\n")
-
-    if args.min_rdp_bootstrap is None and args.min_blast_length is None and args.max_blast_evalue is None and args.min_blast_identity is None and args.min_blast_coverage is None and args.ignore_blast_taxa is None:
-        raise_exception(Exception("\n\n#ERROR : You need to specify at least on filtering criteria\n\n"))
-
+    
     if not args.delete and not args.mask:
         raise_exception( argparse.ArgumentTypeError("\n\n#ERROR : You must precise if you want to mask affiliations of delete OTU with --mask or --delete options.\n\n"))
 
-    if args.min_rdp_bootstrap is None and args.min_blast_identity is None and args.min_blast_coverage is None and args.max_blast_evalue is None and args.min_blast_length is None and len(args.ignore_blast_taxa) == 0:
-        raise_exception( argparse.ArgumentTypeError( "\n\n#ERROR : At least one filter must be set to run " + os.path.basename(sys.argv[0]) + "\n\n"))
+    if args.min_rdp_bootstrap is None and args.min_blast_length is None and args.max_blast_evalue is None and args.min_blast_identity is None and args.min_blast_coverage is None:
+        if args.ignore_blast_taxa is None and args.keep_blast_taxa is None:
+            raise_exception(Exception("\n\n#ERROR : You need to specify at least on filtering criteria\n\n"))
+        elif args.ignore_blast_taxa is not None and len(args.ignore_blast_taxa) == 0:
+            raise_exception( argparse.ArgumentTypeError( "\n\n#ERROR : At least one filter must be set to run " + os.path.basename(sys.argv[0]) + "\n\n"))
+        elif args.keep_blast_taxa is not None and len(args.keep_blast_taxa) == 0:
+            raise_exception( argparse.ArgumentTypeError( "\n\n#ERROR : At least one filter must be set to run " + os.path.basename(sys.argv[0]) + "\n\n"))
     
     in_biom = BiomIO.from_json( args.input_biom )
 
@@ -824,16 +847,13 @@ if __name__ == '__main__':
             raise_exception( argparse.ArgumentTypeError( "\n\n#ERROR : The BIOM input does not contain the metadata 'blast_affiliations'. You cannot use the parameter '--max-blast-coverage' on this file.\n\n" ))
         if args.ignore_blast_taxa is not None:
             raise_exception( argparse.ArgumentTypeError( "\n\n#ERROR : The BIOM input does not contain the metadata 'blast_affiliations'. You cannot use the parameter '--ignore-blast-taxa' on this file.\n\n" ))
-     
+        if args.keep_blast_taxa is not None:
+            raise_exception( argparse.ArgumentTypeError( "\n\n#ERROR : The BIOM input does not contain the metadata 'blast_affiliations'. You cannot use the parameter '--keep-blast-taxa' on this file.\n\n" ))     
     # Control non empty ignore-blast-taxa:
     if args.ignore_blast_taxa is not None:
-        temp = [ i.strip() for i in args.ignore_blast_taxa if i.strip() != '']
-        if len(temp) == 0:
-            raise_exception( argparse.ArgumentTypeError( "\n\n#ERROR : ignore-blast-taxa list need to be non empty strings.\n\n" ))
-        else:
-            if len(temp) != len(args.ignore_blast_taxa):
-                args.ignore_blast_taxa = temp
-                Logger.static_write(args.log_file, "WARNING : empty string in ignore-blast-taxa option have been removed, here is the updated list that will be take into account: \"" + '\" \"'.join(args.ignore_blast_taxa) + "\n\n")
+        checkBlastTaxa(args.ignore_blast_taxa)
+    if args.keep_blast_taxa is not None:
+        checkBlastTaxa(args.keep_blast_taxa)       
 
     for observation in in_biom.get_observations():
         taxonomy = observation['metadata']['blast_taxonomy']
