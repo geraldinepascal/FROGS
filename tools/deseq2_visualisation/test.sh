c146c442735e0caa383901e45460ef04129ceb0e
vindarbot <vincent.darbot@inrae.fr>
2023-02-14 09:50:07 +0100

    deseq2_visualisation: udpating test.sh

--- tools/deseq2_visualisation/test.sh
+++ tools/deseq2_visualisation/test.sh
@@ -12,7 +12,7 @@ else
 fi
 
 # test on quantitative variables
-python deseq2_visualisation.py --phyloseqData  data.Rdata -a OTU --dds dds_EnvType.rdata \
+python deseq2_visualisation.py --abundanceData  data/phyloseq.Rdata -a OTU --dds data/EnvType_DESeq_dds.Rdata \
                             --var EnvType --mod1 SaumonFume --mod2 DesLardons \
-                            --log-file test/deseq2_preprocess_EnvType.log \
-                            --html test/EnvType_DesLardons_SaumonFume.nb.html 
+                            --log-file test/deseq2_preprocess_EnvType.log  \
+                            --html test/EnvType_DesLardons_SaumonFume.nb.html --debug 
