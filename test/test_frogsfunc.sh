c38824f62642304292f2d0ddabf0e9fd2c25c799
vindarbot <vincent.darbot@inrae.fr>
2023-03-02 12:06:34 +0100

    UPDATING test_frogsfunc.sh

--- test/test_frogsfunc.sh
+++ test/test_frogsfunc.sh
@@ -20,13 +20,14 @@ frogsfunc_placeseqs.py \
  --input-fasta data/frogsfunc.fasta \
  --input-biom data/frogsfunc.biom \
  --placement-tool sepp \
- --out-tree $out_dir/25-frogsfunc_placeseqs_tree.nwk \
+ --output-tree  $out_dir/25-frogsfunc_placeseqs_tree.nwk \
  --excluded $out_dir/25-frogsfunc_placeseqs_excluded.txt \
- --insert-fasta $out_dir/25-frogsfunc_placeseqs.fasta \
- --insert-biom $out_dir/25-frogsfunc_placeseqs.biom \
+ --output-fasta $out_dir/25-frogsfunc_placeseqs.fasta \
+ --output-biom $out_dir/25-frogsfunc_placeseqs.biom \
  --closests-ref $out_dir/25-frogsfunc_placeseqs_closests_ref_sequences.txt \
  --log-file $out_dir/25-frogsfunc_placeseqs.log \
- --html $out_dir/25-frogsfunc_placeseqs_summary.html
+ --output-marker $out_dir/25-frogsfunc_marker.tsv \
+ --summary $out_dir/25-frogsfunc_placeseqs_summary.html
 
 if [ $? -ne 0 ]
 then
@@ -34,34 +35,23 @@ then
     exit 1;
 fi
 
-echo "Step frogsfunc_copynumbers `date`"
-
-frogsfunc_copynumbers.py \
- --input-biom $out_dir/25-frogsfunc_placeseqs.biom \
- --tree $out_dir/25-frogsfunc_placeseqs_tree.nwk \
- --output-marker $out_dir/26-frogsfunc_copynumbers_marker.tsv \
- --output-function $out_dir/26-frogsfunc_copynumbers_predicted_functions.tsv \
- --log-file $out_dir/26-frogsfunc_copynumbers.log \
- --html $out_dir/26-frogsfunc_copynumbers_summary.html
-
-if [ $? -ne 0 ]
-then
-    echo "Error in frogsfunc_copynumbers " >&2
-    exit 1;
-fi
-
 echo "Step frogsfunc_functions `date`"
 
 frogsfunc_functions.py \
  --input-biom $out_dir/25-frogsfunc_placeseqs.biom \
- --function $out_dir/26-frogsfunc_copynumbers_predicted_functions.tsv \
- --marker $out_dir/26-frogsfunc_copynumbers_marker.tsv \
- --function-abund $out_dir/27-frogsfunc_functions_unstrat.tsv \
- --seqtab $out_dir/27-frogsfunc_functions_marker_norm.tsv \
- --weighted $out_dir/27-frogsfunc_functions_weighted_nsti.tsv \
- --excluded $out_dir/27-frogsfunc_functions_excluded.txt \
- --log-file $out_dir/27-frogsfunc_functions.log \
- --html $out_dir/27-frogsfunc_functions_summary.html
+ --input-fasta $out_dir/25-frogsfunc_placeseqs.fasta \
+ --input-tree $out_dir/25-frogsfunc_placeseqs_tree.nwk \
+ --marker-type 16S \
+ --input-marker $out_dir/25-frogsfunc_marker.tsv \
+ --output-function-abund $out_dir/26-frogsfunc_functions_unstrat.tsv \
+ --output-otu-norm $out_dir/26-frogsfunc_functions_marker_norm.tsv \
+ --output-weighted $out_dir/26-frogsfunc_functions_weighted_nsti.tsv \
+ --output-excluded $out_dir/26-frogsfunc_functions_excluded.txt \
+ --output-fasta $out_dir/26-frogsfunc_function.fasta \
+ --output-biom $out_dir/26-frogsfunc_function.biom \
+ --log-file $out_dir/26-frogsfunc_functions.log \
+ --summary $out_dir/26-frogsfunc_functions_summary.html
+
 
 if [ $? -ne 0 ]
 then
@@ -72,10 +62,17 @@ fi
 echo "Step frogsfunc_pathways `date`"
 
 frogsfunc_pathways.py \
- --input-file $out_dir/27-frogsfunc_functions_unstrat.tsv \
- --pathways-abund $out_dir/28-frogsfunc_pathways_unstrat.tsv \
- --log-file $out_dir/28-frogsfunc_pathways.log \
- --html $out_dir/28-frogsfunc_pathways_summary.html
+ --input-file $out_dir/26-frogsfunc_functions_unstrat_EC.tsv \
+ --normalisation \
+ --per-sequence-contrib \
+ --per-sequence-abun $out_dir/26-frogsfunc_functions_marker_norm.tsv \
+ --per-sequence-function $out_dir/EC_copynumbers_predicted.tsv \
+ --output-pathways-abund $out_dir/27-frogsfunc_pathways_unstrat.tsv \
+ --output-pathways-contrib $out_dir/27-frogsfunc_pathways_strat.tsv \
+ --output-pathways-predictions $out_dir/27-frogsfunc_pathways_predictions.tsv \
+ --output-pathways-abund-per-seq $out_dir/27-frogsfunc_pathways_unstrat_per_seq.tsv \
+ --log-file $out_dir/27-frogsfunc_pathways.log \
+ --summary  $out_dir/27-frogsfunc_pathways_summary.html
 
 if [ $? -ne 0 ]
 then
