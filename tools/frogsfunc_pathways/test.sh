903e1b922a888d1e10fecad6106b321d063f96dd
mariabernard <maria.bernard@inare.fr>
2023-03-07 15:33:10 +0100

    re-add test data and test.sh

---
+++ tools/frogsfunc_pathways/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+FROGS_DIR=`dirname $(dirname $(pwd))`
+export PATH=$FROGS_DIR/libexec:$PATH
+export PYTHONPATH=$FROGS_DIR/lib:$PYTHONPATH
+export DESCRIPTION_FILE=$FROGS_DIR/frogsfunc_suppdata/pathways_description_file.txt.gz
+export PATHWAYS_HIERARCHY_FILE=$FROGS_DIR/frogsfunc_suppdata/pathways_hierarchy.tsv
+
+# Create output folder
+if [ ! -d "test" ]
+then
+    mkdir test
+fi
+
+./frogsfunc_pathways.py \
+    --input-file data/26-frogsfunc_functions_unstrat_EC.tsv \
+    --normalisation \
+    --per-sequence-contrib \
+    --per-sequence-abun data/26-frogsfunc_functions_marker_norm.tsv \
+    --per-sequence-function data/EC_copynumbers_predicted.tsv \
+    --output-pathways-abund test/27-frogsfunc_pathways_unstrat.tsv \
+    --output-pathways-contrib test/27-frogsfunc_pathways_strat.tsv \
+    --output-pathways-predictions test/27-frogsfunc_pathways_predictions.tsv \
+    --output-pathways-abund-per-seq test/27-frogsfunc_pathways_unstrat_per_seq.tsv \
+    --summary test/27-frogsfunc_pathways_summary.html
---
+++ tools/phyloseq_clustering/test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+FROGS_DIR=`dirname $(dirname $(pwd))`
+export PATH=$FROGS_DIR/libexec:$PATH
+export PYTHONPATH=$FROGS_DIR/lib:$PYTHONPATH
+
+if [ -d test ]
+then
+  rm -r test/*
+else
+  mkdir test
+fi 
+
+./phyloseq_clustering.py \
+    --rdata data/data.Rdata \
+    --varExp EnvType \
+    --distance-matrix data/Unifrac.tsv \
+    --html test/phylo_clustering.nb.html \
+    -l test/phylo_clustering.log
