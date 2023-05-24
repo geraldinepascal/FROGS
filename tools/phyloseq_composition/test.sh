bc30e63f1fd88caa4e2ce0d5f9675117066ede95
vindarbot <vincent.darbot@inrae.fr>
2023-05-24 17:12:26 +0200

    add test files and data

---
+++ tools/phyloseq_composition/test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+FROGS_DIR=`dirname $(dirname $(pwd))`
+export PATH=$FROGS_DIR/libexec:$PATH
+export PYTHONPATH=$FROGS_DIR/lib:$PYTHONPATH
+
+if [ -d test ]
+then
+	rm -r test/*
+else
+	mkdir test
+fi 
+ ./phyloseq_composition.py -r data/data.Rdata \
+	-v EnvType -r1 Kingdom -s1 Bacteria -r2 Phylum -n 9 \
+	-l test/phylo_compo.log -o test/phylo_compo.nb.html
---
+++ tools/phyloseq_import/test.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+FROGS_DIR=`dirname $(dirname $(pwd))`
+export PATH=$FROGS_DIR/libexec:$FROGS_DIR/app:$PATH
+export PYTHONPATH=$FROGS_DIR/lib:$PYTHONPATH
+
+# Create output folder
+if [ ! -d "test" ]
+then
+    mkdir test
+else
+    rm -r test/*
+fi
+
+# without normalisation and default ranks
+mkdir -p test/
+./phyloseq_import_data.py  \
+	-n \
+	-b data/chaillou.biom \
+	-s data/sample_data.tsv \
+	-t data/tree.nwk \
+	--rdata test/phylo_import.Rdata -o test/phylo_import.nb.html -l test/phylo_import.log
+
+# on unstandardized biom
+./phyloseq_import_data.py  \
+	-n \
+	-b data/frogs.biom \
+	-s data/frogs_sample_data.tsv \
+	--rdata test/frogs_import.Rdata -o test/frogs_import.nb.html -l test/frogs_import.log
