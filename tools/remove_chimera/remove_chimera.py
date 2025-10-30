4d3b66f4c628fedfd2d3cb188f4103a46921c7e3
Olivier Rué <olivier.rue@inrae.fr>
2025-10-30 22:10:02 +0100

    remove chimera html

--- tools/remove_chimera/remove_chimera.py
+++ tools/remove_chimera/remove_chimera.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-__author__ = 'Frederic Escudie - Genotoul/MIAT & Maria Bernard - SIGENAE/GABI'
+__author__ = 'Frederic Escudie - Genotoul/MIAT & Maria Bernard - SIGENAE/GABI & Olivier Rué - Migale/MaIAGE'
 __copyright__ = 'Copyright (C) 2024 INRAE'
 __license__ = 'GNU General Public License'
 __version__ = '5.1.0'
@@ -22,6 +22,10 @@ LIB_DIR = os.path.abspath(os.path.join(os.path.dirname(CURRENT_DIR), "lib"))
 sys.path.append(LIB_DIR)
 if os.getenv('PYTHONPATH') is None: os.environ['PYTHONPATH'] = LIB_DIR
 else: os.environ['PYTHONPATH'] = LIB_DIR + os.pathsep + os.environ['PYTHONPATH']
+# THEME
+THEME_DIR = os.path.abspath(os.path.join(os.path.dirname(CURRENT_DIR), "static"))
+if not os.path.exists(THEME_DIR):
+    THEME_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(CURRENT_DIR)), "static"))
 
 from frogsUtils import *
 from frogsSequenceIO import *
@@ -149,8 +153,8 @@ def write_summary( summary_file, results_chimera, depth_file, biom_file):
             if in_detection_metrics:
                 if section_first_line:
                     line_fields = line[1:].split("\t")[1:]
-                    line_fields.insert(1,"%  Clusters kept")
-                    line_fields.insert(3,"%  Cluster abundance kept")
+                    line_fields.insert(1,"%  of clusters kept")
+                    line_fields.insert(3,"%  of cluster abundance kept")
                     detection_categories = line_fields
                     section_first_line = False
                 else:
@@ -215,12 +219,27 @@ def write_summary( summary_file, results_chimera, depth_file, biom_file):
             'own_observations': own_observations
         }
     del biom
-
+    
     # Write
     FH_summary_tpl = open( os.path.join(CURRENT_DIR, "remove_chimera_tpl.html") )
     FH_summary_out = open( summary_file, "wt" )
+    # Load shared JS
+    with open(os.path.join(THEME_DIR, "js", "theme.js")) as f:
+        theme_js = f.read()
+    with open(os.path.join(THEME_DIR, "js", "utils.js")) as f:
+        utils_js = f.read()
+    # Load shared CSS
+    with open(os.path.join(THEME_DIR, "css", "common.css")) as f:
+        common_css = f.read()
     for line in FH_summary_tpl:
-        if "###DETECTION_CATEGORIES###" in line:
+        if "###IMPORT_CSS###" in line:
+            line = line.replace("###IMPORT_CSS###", f"<style type='text/css'>{common_css}</style>")
+        elif "###IMPORT_JS_UTILS###" in line:
+            # injection du JS inline
+            line = line.replace("###IMPORT_JS_UTILS###", f"<script>\n{utils_js}</script>")
+        elif "###IMPORT_JS_THEME###" in line:
+            line = line.replace("###IMPORT_JS_THEME###", f"<script>\n{theme_js}</script>")
+        elif "###DETECTION_CATEGORIES###" in line:
             line = line.replace( "###DETECTION_CATEGORIES###", json.dumps(detection_categories) )
         elif "###DETECTION_DATA###" in line:
             line = line.replace( "###DETECTION_DATA###", json.dumps(detection_data) )
