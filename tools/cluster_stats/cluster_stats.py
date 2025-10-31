acf3805ab64c82ffaa1bfc5e7d0c5c7a83c39f99
Olivier Rué <olivier.rue@inrae.fr>
2025-10-31 07:42:00 +0100

    cluster stats

--- tools/cluster_stats/cluster_stats.py
+++ tools/cluster_stats/cluster_stats.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-__author__ = 'Frédéric Escudié - Genotoul/MIAT'
-__copyright__ = 'Copyright (C) 2024 INRAE'
+__author__ = 'Frederic Escudie - Genotoul/MIAT & Maria Bernard - SIGENAE/GABI & Olivier Rué - Migale/MaIAGE'
+__copyright__ = 'Copyright (C) 2025 INRAE'
 __license__ = 'GNU General Public License'
 __version__ = '5.1.0'
 __email__ = 'frogs-support@inrae.fr'
@@ -21,6 +21,10 @@ LIB_DIR = os.path.abspath(os.path.join(os.path.dirname(CURRENT_DIR), "lib"))
 sys.path.append(LIB_DIR)
 if os.getenv('PYTHONPATH') is None: os.environ['PYTHONPATH'] = LIB_DIR
 else: os.environ['PYTHONPATH'] = LIB_DIR + os.pathsep + os.environ['PYTHONPATH']
+# THEME
+THEME_DIR = os.path.abspath(os.path.join(os.path.dirname(CURRENT_DIR), "static"))
+if not os.path.exists(THEME_DIR):
+    THEME_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(CURRENT_DIR)), "static"))
 
 from frogsUtils import *
 from frogsBiom import BiomIO
@@ -156,8 +160,23 @@ def write_summary( summary_file, input_biom, depth_file, classif_file=None ):
     # Write
     FH_summary_tpl = open( os.path.join(CURRENT_DIR, "cluster_stats_tpl.html") )
     FH_summary_out = open( summary_file, "wt" )
+    # Load shared JS
+    with open(os.path.join(THEME_DIR, "js", "theme.js")) as f:
+        theme_js = f.read()
+    with open(os.path.join(THEME_DIR, "js", "utils.js")) as f:
+        utils_js = f.read()
+    # Load shared CSS
+    with open(os.path.join(THEME_DIR, "css", "common.css")) as f:
+        common_css = f.read()
     for line in FH_summary_tpl:
-        if "###CLUSTERS_SIZES###" in line:
+        if "###IMPORT_CSS###" in line:
+            line = line.replace("###IMPORT_CSS###", f"<style type='text/css'>{common_css}</style>")
+        elif "###IMPORT_JS_UTILS###" in line:
+            # injection du JS inline
+            line = line.replace("###IMPORT_JS_UTILS###", f"<script>\n{utils_js}</script>")
+        elif "###IMPORT_JS_THEME###" in line:
+            line = line.replace("###IMPORT_JS_THEME###", f"<script>\n{theme_js}</script>")
+        elif "###CLUSTERS_SIZES###" in line:
             line = line.replace( "###CLUSTERS_SIZES###", json.dumps(clusters_size) )
         elif "###DATA_COUNTS###" in line:
             line = line.replace( "###DATA_COUNTS###", json.dumps(counts) )
